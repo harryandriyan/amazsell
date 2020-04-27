3fe4e3a1c118bb99ca37e795765eab36acb30c03
Harry Andriyan <harry.andriyan@gmail.com>
2020-04-27 12:34:24 +0700

    fix(app): use .env

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const mongoUri = 'mongodb+srv://root:123qwepoi@amazselldata-bbyi2.mongodb.net/amazsell_db?retryWrites=true&w=majority'
+const hostDb = process.env.MONGO_DATABASE_HOST
+const database = process.env.MONGO_DATABASE_DB
+const userDb = process.env.MONGO_DATABASE_USER
+const passDb = process.env.MONGO_DATABASE_PASS
+
+const mongoUri = `mongodb+srv://${userDb}:${passDb}@${hostDb}/${database}?retryWrites=true&w=majority`
 mongoose
   .connect(mongoUri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
   })
   .catch(e => {
     console.error('Connection error', e.message)
